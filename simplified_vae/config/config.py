fix KL loss

simplified_vae/config/config.py
@@ -1,6 +1,6 @@
 import pydantic
 import torch
-
+from tensorboardX import SummaryWriter
 
 class BaseModel(pydantic.BaseModel, extra=pydantic.Extra.forbid):
     """Disallow extra arguments to init"""
@@ -29,11 +29,11 @@ class RewardDecoderConfig(BaseModel):
 
 class TrainingConfig:
 
-    lr: float = 0.0003
+    lr: float = 0.001
     batch_size: int = 32
-    state_reconstruction_loss_weight: float = 1
-    reward_reconstruction_loss_weight: float = 1
-    kl_loss_weight: float = 1
+    state_reconstruction_loss_weight: float = 1.0
+    reward_reconstruction_loss_weight: float = 1.0
+    kl_loss_weight: float = 0.1
     pretrain_episodes = 100
 
 
@@ -48,6 +48,8 @@ class Config():
     env_name: str = 'HalfCheetah-v3'
     device: int = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     seed: int = 0
+
+    logger = SummaryWriter(f'runs/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_VAE')
 
     epiosde_num = 100
 

simplified_vae/models/encoder.py
@@ -53,8 +53,10 @@ class RNNEncoder(nn.Module):
             reparameterise(self, mu, logvar):
 
         std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
+        noise = torch.randn_like(std).to(self.device)
+        z = mu + noise * std
+
+        return z
 
     def forward(self, obs, actions, rewards, hidden_state = None):
 

simplified_vae/utils/losses.py
@@ -21,18 +21,18 @@ def compute_kl_loss(latent_mean, latent_logvar):
     # -- KL divergence
     gauss_dim = latent_mean.shape[-1]
 
-    # add the gaussian prior
-    all_means = torch.cat((torch.zeros(1, *latent_mean.shape[1:]).to(device), latent_mean))
-    all_logvars = torch.cat((torch.zeros(1, *latent_logvar.shape[1:]).to(device), latent_logvar))
-
     # https://arxiv.org/pdf/1811.09975.pdf
     # KL(N(mu,E)||N(m,S)) = 0.5 * (log(|S|/|E|) - K + tr(S^-1 E) + (m-mu)^T S^-1 (m-mu)))
-    mu = all_means[1:]
-    m = all_means[:-1]
-    logE = all_logvars[1:]
-    logS = all_logvars[:-1]
-    kl_divergences = 0.5 * (torch.sum(logS, dim=-1) - torch.sum(logE, dim=-1) - gauss_dim + torch.sum(
-        1 / torch.exp(logS) * torch.exp(logE), dim=-1) + ((m - mu) / torch.exp(logS) * (m - mu)).sum(dim=-1))
+
+    mu = latent_mean[:, 1:, :] # curr_posterior_mean
+    m = latent_mean[:, :-1, :] # prev_posterior_mean
+
+    logE = latent_logvar[:, 1:, :] # curr_posterior_logvar
+    logS = latent_logvar[:,:-1, :] # prev_posterior_logvar
+
+    kl_divergences = 0.5 * (torch.sum(logS, dim=-1) - torch.sum(logE, dim=-1) - gauss_dim +
+                            torch.sum(torch.exp(logE) / torch.exp(logS), dim=-1) +
+                            ((m - mu) / torch.exp(logS) * (m - mu)).sum(dim=-1))
 
     return kl_divergences
 