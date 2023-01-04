98b2efce6ae3712f2035d6853e4beb164061955e
goy1tv <yuval.goldfracht@il.bosch.com>
2023-01-04 16:00:08 +0200

    TWR implementation - WIP

--- simplified_vae/config/config.py
+++ simplified_vae/config/config.py
@@ -99,6 +99,32 @@ class ClusteringConfig(BaseModel):
     clusters_num = 50
 
 
+class TWRConfig(BaseModel):
+
+    obs_shape: int = 10
+    latents_shape: int = 5
+    hidden_size: int = 32
+    n_layers: int = 5
+    init_std: int = - 1
+    loss_type: str = 'KL' # 'KL', 'exp' or 'sqrt'
+
+    n_trajectories: int = 1
+    length: int = 1000
+    change_point: int = 500
+
+    t_init: int = 50
+    t_end: int = 1000
+    n_epochs: int = 30
+    batch_size: int = 32
+    epsilon: float = .01 # speed of annealing
+    t_min: int = 10
+    annealing: bool = True
+
+    annealing_coef: float = .01
+    lr: float = 0.001
+    train_eps: float = 1e-7
+    seed: int = 0
+
 class Config:
 
     env_name: str = 'HalfCheetah-v3'
@@ -114,4 +140,4 @@ class Config:
     test_buffer: TestBufferConfig = TestBufferConfig()
 
     clustering: ClusteringConfig = ClusteringConfig()
-
+    twr: TWRConfig = TWRConfig()
