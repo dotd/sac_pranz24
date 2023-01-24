69fea3ebfb9e3d06c93c6619d0cf14db2c3516e5
Esther Derman <boccara.esther@gmail.com>
2023-01-24 03:48:13 -0500

    add wd to runner

--- simplified_vae/main_sac.py
+++ simplified_vae/main_sac.py
@@ -1,7 +1,44 @@
+import os
+import wandb
+import sys
+
+# from simplified_vae.config.config import Config
+# from simplified_vae.utils.env_utils import make_stationary_env, set_seed, make_toggle_env, make_fixed_toggle_env
+# from simplified_vae.utils.poc_trainer import POCTrainer
+
+# Add script directory to sys.path.
+def GetScriptDirectory():
+    if hasattr(GetScriptDirectory, "dir"):
+        return GetScriptDirectory.dir
+    module_path = ""
+    try:
+        # The easy way. Just use __file__.
+        # Unfortunately, __file__ is not available when cx_Freeze is used or in IDLE.
+        module_path = __file__
+    except NameError:
+        if len(sys.argv) > 0 and len(sys.argv[0]) > 0 and os.path.isabs(sys.argv[0]):
+            module_path = sys.argv[0]
+        else:
+            module_path = os.path.abspath(inspect.getfile(GetScriptDirectory))
+            if not os.path.exists(module_path):
+                # If cx_Freeze is used the value of the module_path variable at this point is in the following format.
+                # {PathToExeFile}\{NameOfPythonSourceFile}.
+                # This makes it necessary to strip off the file name to get the correct
+                # path.
+                module_path = os.path.dirname(module_path)
+    GetScriptDirectory.dir = os.path.dirname(module_path)
+    return GetScriptDirectory.dir
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+sys.path.append(os.path.join(GetScriptDirectory(), "lib"))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'/simplified_vae/')))
+# print(os.getcwd())
+
 from simplified_vae.config.config import Config
 from simplified_vae.utils.env_utils import make_stationary_env, set_seed, make_toggle_env, make_fixed_toggle_env
 from simplified_vae.utils.poc_trainer import POCTrainer
-import wandb
 
 def main():
 
