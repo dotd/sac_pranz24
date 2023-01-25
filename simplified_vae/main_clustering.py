24b2ab2b16c26bf66666175d51ac7669d58dfe69
yuvalg1987 <yuvalg1987@gmail.com>
2023-01-25 09:29:02 -0500

    add a possion fixed environewnt

--- simplified_vae/main_clustering.py
+++ simplified_vae/main_clustering.py
@@ -17,6 +17,8 @@ def run_cusum(curr_transitions, markov_dist_0, markov_dist_1):
 
     n_c, s_k, S_k, g_k = 0, [], [], []
     sample_len = len(curr_transitions)
+    done = False
+
     for k in range(sample_len):
 
         curr_sample = curr_transitions[k, :]
@@ -30,17 +32,25 @@ def run_cusum(curr_transitions, markov_dist_0, markov_dist_1):
         min_S_k = min(S_k)
         g_k.append(S_k[-1] - min_S_k)
 
-        if g_k[-1] > 10:
-            n_c = S_k.index(min(S_k))
-            # break
+        if g_k[-1] > 15 and not done:
+            n_c = k #S_k.index(min(S_k))
+            done = True
+            #break
 
     print(f'n_c = {n_c}')
     plt.figure(), plt.plot(g_k), plt.show(block=True)
+    return n_c, g_k
 
 def main():
 
     ## Init config
+
+    # checkpoint_path = 'runs/2023-01-02_09-12-57_VAE/model_best.pth.tar' # Our approach
+    checkpoint_path = 'runs/2023-01-23_15-54-51_VAE/model_best.pth.tar' # VARIBAD with non-stationary trajectories
+    # checkpoint_path = 'runs/2023-01-24_09-06-11_VAE/model_best.pth.tar' # VARIBAD with stationary trajectories
+
     config = Config()
+    config.model.checkpoint_path = checkpoint_path
     rg = set_seed(config.seed)
 
     # Init Env
@@ -48,14 +58,9 @@ def main():
     obs_dim: int = env.observation_space.shape[0]
     discrete = isinstance(env.action_space, gym.spaces.Discrete)
     action_dim: int = env.action_space.n if discrete else env.action_space.shape[0]
-    episode_num = 2000
+    episode_num = 200
     max_episode_len = 100
     clusters_num = 10
-
-    # Init model
-    # checkpoint_path = 'runs/2023-01-01_11-55-39_VAE/model_best.pth.tar'
-    # checkpoint_path = 'runs/2023-01-02_11-54-10_VAE/model_best.pth.tar'
-    checkpoint_path = 'runs/2023-01-02_09-12-57_VAE/model_best.pth.tar'
 
     model, epoch, loss = init_model(config=config,
                                     obs_dim=obs_dim,
@@ -84,7 +89,6 @@ def main():
                                     episode_len=max_episode_len,
                                     env_change_freq=episode_num)
 
-
     model.eval()
     with torch.no_grad():
         obs_0, actions_0, rewards_0, next_obs_0 = test_buffer.sample_section(start_idx=0,
@@ -98,7 +102,6 @@ def main():
         latent_sample_0, latent_mean_0, latent_logvar_0, output_0, hidden_state = model.encoder(obs=obs_0_d, actions=actions_0_d, rewards=rewards_0_d)
         latent_sample_1, latent_mean_1, latent_logvar_1, output_1, hidden_state = model.encoder(obs=obs_1_d, actions=actions_1_d, rewards=rewards_1_d)
 
-        # latent_mean = latent_mean_0 # TODO decide on which data should we do the clustering
         latent_mean = torch.cat([latent_mean_0, latent_mean_1], dim=0)
 
         latent_mean_h = latent_mean.detach().cpu().numpy()
@@ -166,11 +169,11 @@ def main():
 
         curr_labels = kmeans.predict(sample_joint_trajectory)
         curr_transitions = np.stack([curr_labels[:-1], curr_labels[1:]], axis=1)
-        run_cusum(curr_transitions, markov_dist_0, markov_dist_1)
+        n_c, g_k = run_cusum(curr_transitions, markov_dist_0, markov_dist_1)
 
         # Single Trajectory
         obs_0_d, actions_0_d, rewards_0_d, next_obs_0_d = all_to_device(obs_0, actions_0, rewards_0, next_obs_0, device=config.device)
-        latent_sample_0, latent_mean_0, latent_logvar_0, output_0 = model.encoder(obs=obs_0_d, actions=actions_0_d, rewards=rewards_0_d)
+        latent_sample_0, latent_mean_0, latent_logvar_0, output_0, hidden_state = model.encoder(obs=obs_0_d, actions=actions_0_d, rewards=rewards_0_d)
 
         sample_joint_trajectory = latent_mean_0[0, ...]
         sample_joint_trajectory = sample_joint_trajectory.detach().cpu().numpy()
@@ -179,7 +182,7 @@ def main():
         curr_transitions = np.stack([curr_labels[:-1], curr_labels[1:]], axis=1)
         run_cusum(curr_transitions, markov_dist_0, markov_dist_1)
 
-
+        a = 1
 
 if __name__ == '__main__':
     main()
